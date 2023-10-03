0cd4c3245771ca6874a9a41c56edd35e31d20e84
Carrington <Manyuchi>
2023-10-03 10:07:04 +0200

    Initial Commit

---
+++ TabBarVCTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBarVCTest
+//
+//  Created by DA MAC M1 157 on 2023/10/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
